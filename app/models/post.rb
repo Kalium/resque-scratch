8d507baa2d56d99722a31e4783bf53e77f3886c9
Jon Frisby <jfrisby@mrjoy.com>
2012-03-06 18:34:03 -0800

    HOOKS!

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,29 @@
 class Post < ActiveRecord::Base
+  after_initialize do
+    logger.debug("after_initialize")
+  end
+
+  before_validation do
+    logger.debug("before_validation")
+  end
+  after_validation do
+    logger.debug("after_validation")
+  end
+
+  before_create do
+    logger.debug("before_create")
+  end
+  after_create do
+    logger.debug("after_create")
+  end
+
+  before_save do
+    logger.debug("before_save")
+  end
   after_save do
+    logger.debug("after_save")
     after_transaction do
+      logger.debug("after_save>after_transaction")
       Resque::Job.create("my_queue", "Jobs::Test::Dummy", id)
     end
   end
