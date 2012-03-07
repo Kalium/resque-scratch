a46a2c340f4f968bc6ce9cf263cedf7be3003486
Jeremy Wagner-Kaiser <jeremy@cloudability.com>
2012-03-06 17:42:07 -0800

    Moving day.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResqueApp::Application.config.secret_token = 'c5db8116a171a9520ac62daae58e5f6d1098f85aa763b180b206943c225db77cf3dbcf932b72573a3e5d2ac62486a9ac60a10e80cc2662739bc468fd1197fbc8'
