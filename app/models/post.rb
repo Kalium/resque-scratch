5053801c932515bdcf81ad93c049f6823fb1e1e6
Jon Frisby <jfrisby@mrjoy.com>
2012-03-06 18:26:42 -0800

    Fixing our race condition.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   after_save do
-    Resque::Job.create("my_queue", "Jobs::Test::Dummy", id)
+    after_transaction do
+      Resque::Job.create("my_queue", "Jobs::Test::Dummy", id)
+    end
   end
 end
