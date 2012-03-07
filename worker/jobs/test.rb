9b15fbf35212ec3dc601d7558e2d1686c2eb5fd1
Jon Frisby <jfrisby@mrjoy.com>
2012-03-06 18:12:53 -0800

    Show stuff about the post.

--- worker/jobs/test.rb
+++ worker/jobs/test.rb
@@ -2,6 +2,9 @@ module Jobs
   module Test
     class Dummy
       def self.perform(post_id)
+        STDOUT.puts "GOT: #{post_id}..."
+        post = Post.find(post_id)
+        STDOUT.puts "TITLE: '#{post.title}'"
       end
     end
   end
